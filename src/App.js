13a3289aa93596e698500998a18ffc5f2c165641
Hayk Mnatsakanyan <Zoneam@gmail.com>
2022-01-18 15:56:33 -0800

    initial push with axios and react card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import DisplayCard from './components/Card';
+import axios from 'axios';
+import { useEffect } from 'react';
+const API = "QDEUlIuFI1qnLu24hs2yikNPuWQdpXAAZVVuomhH"
 
 function App() {
+  
+useEffect(()=>{
+  axios('https://api.nasa.gov/planetary/apod?api_key=QDEUlIuFI1qnLu24hs2yikNPuWQdpXAAZVVuomhH&count=10')
+  .then(response => {
+  console.log(response.data)  
+  })
+})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <DisplayCard >
+      
+      </DisplayCard>
     </div>
   );
 }
