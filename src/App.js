17584af2193764c48e24852f04dfa6ea73c5a324
Hayk Mnatsakanyan <Zoneam@gmail.com>
2022-01-18 17:02:16 -0800

    fetching data and mapping over to display cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import './App.css';
 import DisplayCard from './components/Card';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 const API = "QDEUlIuFI1qnLu24hs2yikNPuWQdpXAAZVVuomhH"
 
+
 function App() {
-  
-useEffect(()=>{
-  axios('https://api.nasa.gov/planetary/apod?api_key=QDEUlIuFI1qnLu24hs2yikNPuWQdpXAAZVVuomhH&count=10')
-  .then(response => {
-  console.log(response.data)  
-  })
-})
+
+const [data, setData] = useState([]);
+
+useEffect(async () => {
+  const result = await  axios('https://api.nasa.gov/planetary/apod?api_key=QDEUlIuFI1qnLu24hs2yikNPuWQdpXAAZVVuomhH&count=10');
+  setData(result.data);
+  console.log(result.data)
+},[]);
+
 
   return (
     <div className="App">
-
-      <DisplayCard >
-      
-      </DisplayCard>
+      {data.map((singleResponse)=><DisplayCard key={singleResponse.date} data={singleResponse}/>)
+  }
     </div>
   );
 }
