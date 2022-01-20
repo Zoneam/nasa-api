fbef49376e6b42878be12d5399d5b14eebf3d665
Hayk Mnatsakanyan <Zoneam@gmail.com>
2022-01-19 21:13:25 -0800

    date and share to social media button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { MDBCard, MDBCardTitle, MDBCardText, MDBCardBody, MDBCardImage, MDBRow, 
 import './card.css';
 
 const DisplayCard = ({data}) => {
-  const { title , url, explanation } = data;
+  const { title , url, explanation, date, hdurl } = data;
   const [like, setLike] = useState();
 
  function handleLike() {
@@ -30,11 +30,15 @@ const DisplayCard = ({data}) => {
         <MDBCol md='8'>
           <MDBCardBody>
             <MDBCardTitle>{title}</MDBCardTitle>
+            <MDBCardTitle>Capture Date: {date}</MDBCardTitle>
             <MDBCardText>
             {explanation}
             </MDBCardText>
           </MDBCardBody>
-          {like?<i className="bi bi-hand-thumbs-up-fill float-right mr-2 mb-2 thumb" style={{"color": "green", "fontSize":"2rem" }} onClick={handleLike}></i>:<i className="bi bi-hand-thumbs-down-fill float-right mr-2 mb-2 thumb" style={{"color": "red", "fontSize":"2rem" }} onClick={handleLike}></i>}
+          <a href={"https://www.facebook.com/sharer/sharer.php?u=" + hdurl} target="_blank">
+          <button type="button" className="btn btn-info mb-3" >Share Image</button>
+          </a>
+          {like?<i className="bi bi-hand-thumbs-up-fill mr-4 mb-2 thumb" style={{"color": "green", "fontSize":"3rem" }} onClick={handleLike}></i>:<i className="bi bi-hand-thumbs-down-fill  mr-4 mb-2 thumb" style={{"color": "red", "fontSize":"3rem" }} onClick={handleLike}></i>}
         </MDBCol>
       </MDBRow>
     </MDBCard> 
