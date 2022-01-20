334c8d7b0187512ab874bfb0846f60c33b950b35
Hayk Mnatsakanyan <Zoneam@gmail.com>
2022-01-19 16:24:39 -0800

    added react usestate hook to handle like and unlike

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,26 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { MDBCard, MDBCardTitle, MDBCardText, MDBCardBody, MDBCardImage, MDBRow, MDBCol } from 'mdb-react-ui-kit';
 
 const DisplayCard = ({data}) => {
-  const { title , url, explanation} = data;
+  const { title , url, explanation } = data;
+  const [like, setLike] = useState();
 
-  const handleLike = async (e) => {
-    e.preventDefault();
-    const likes = JSON.parse(localStorage.getItem("Likes")) || [];
-    if (!likes.includes(title)){
-      likes.push(title);
-      localStorage.setItem("Likes", JSON.stringify(likes));
-    } else {
-      const i = likes.indexOf(title);
-      likes.splice(i, 1);
-      localStorage.setItem("Likes", JSON.stringify(likes));
+ function handleLike() {
+      const likes = JSON.parse(localStorage.getItem("Likes")) || [];
+      if (!likes.includes(title)){
+        likes.push(title);
+        localStorage.setItem("Likes", JSON.stringify(likes));
+        setLike(true)
+      } else {
+        const i = likes.indexOf(title);
+        likes.splice(i, 1);
+        localStorage.setItem("Likes", JSON.stringify(likes));
+        setLike(false)
+      }
     }
-  }
 
   return (
-
  <MDBCard >
       <MDBRow className='g-0'>
         <MDBCol md='4'>
@@ -31,7 +33,7 @@ const DisplayCard = ({data}) => {
             {explanation}
             </MDBCardText>
           </MDBCardBody>
-          <i className="bi bi-hand-thumbs-up-fill float-right mr-2 mb-2" style={{"color": "red", "fontSize":"2rem" }} onClick={handleLike}></i>
+          {like?<i className="bi bi-hand-thumbs-up-fill float-right mr-2 mb-2" style={{"color": "red", "fontSize":"2rem" }} onClick={handleLike}></i>:<i className="bi bi-hand-thumbs-down-fill float-right mr-2 mb-2" style={{"color": "blue", "fontSize":"2rem" }} onClick={handleLike}></i>}
         </MDBCol>
       </MDBRow>
       
